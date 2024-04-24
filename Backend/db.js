7d63402ebe4f972ff8f3a7eaa4767067f157b1d6
Himanshu Singh <154319555+HimanshuSingh36@users.noreply.github.com>
2024-04-24 23:26:12 +0530

    Update db.js

--- Backend/db.js
+++ Backend/db.js
@@ -1,1 +1,8 @@
+const mongoose = require('mongoose');
+const mongoUri = "mongodb://localhost:27017/inotebook"
 
+async function connectToMongo() {
+    await mongoose.connect(mongoUri).then(()=> console.log("Connected to Mongo Successfully")).catch(err => console.log(err));
+  }
+  
+  module.exports = connectToMongo;
